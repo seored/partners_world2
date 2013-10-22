3f6406a0527c0857d84e58d9d44e2dec30834418
seored <ivan.chumak@bk.ru>
2013-10-22 15:25:38 +0600

    edit index.html
    Добавлена галерея, вместо статичного банера

--- verstka/script/script.js
+++ verstka/script/script.js
@@ -1,4 +1,42 @@
-﻿/*Для menu*/
+﻿function theRotator() {
+	// Устанавливаем прозрачность всех картинок в 0
+	$('div#header_banner ul li').css({opacity: 0.0});
+ 
+	// Берем первую картинку и показываем ее (по пути включаем полную видимость)
+	$('div#header_banner ul li:first').css({opacity: 1.0});
+ 
+	// Вызываем функцию rotate для запуска слайдшоу, 5000 = смена картинок происходит раз в 5 секунд
+	setInterval('rotate()',5000);
+}
+ 
+function rotate() {	
+	// Берем первую картинку
+	var current = ($('div#header_banner ul li.show')?  $('div#header_banner ul li.show') : $('div#header_banner ul li:first'));
+ 
+	// Берем следующую картинку, когда дойдем до последней начинаем с начала
+	var next = ((current.next().length) ? ((current.next().hasClass('show')) ? $('div#header_banner ul li:first') :current.next()) : $('div#header_banner ul li:first'));	
+ 
+	// Расскомментируйте, чтобы показвать картинки в случайном порядке
+	// var sibs = current.siblings();
+	// var rndNum = Math.floor(Math.random() * sibs.length );
+	// var next = $( sibs[ rndNum ] );
+ 
+	// Подключаем эффект растворения/затухания для показа картинок, css-класс show имеет больший z-index
+	next.css({opacity: 0.0})
+	.addClass('show')
+	.animate({opacity: 1.0}, 1500);
+ 
+	// Прячем текущую картинку
+	current.animate({opacity: 0.0}, 1500)
+	.removeClass('show');
+};
+ 
+$(document).ready(function() {		
+	// Запускаем слайдшоу
+	theRotator();
+});
+ 
+/*Для menu*/
   $(document).ready(function(){
 	$(".active").next("li").addClass("nav_next");
 	$(".active").prev("li").addClass("nav_previous");
@@ -302,5 +340,14 @@ $(function(){
 	})
 	$(".new_agency_delete").on("click",function(){
 		$(".wrap_geographi_new_agency").hide();
+	})
+})
+/*Тарифы*/
+$(function(){
+	$(".wrap_rate_row_unit").on("click",function(){
+		$(".wrap_rate_more_info").fadeIn();
+	})
+	$(".rate_more_info_close").on("click",function(){
+		$(".wrap_rate_more_info").fadeOut();
 	})
 })
